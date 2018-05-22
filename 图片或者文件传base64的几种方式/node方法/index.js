f2409346e967ae2b3a59791e17d7b560627c5f50
hswx <597084762@qq.com>
2018-05-22 23:54:34 +0800

    no message

--- 图片或者文件传base64的几种方式/node方法/index.js
+++ 图片或者文件传base64的几种方式/node方法/index.js
@@ -1,3 +1,3 @@
 let fs = require('fs')
-let imageBuf = fs.readFileSync('./../testImg.jpg');
-console.log('data:image/jpeg;base64,'+imageBuf.toString('base64'));
+let imageBuf = fs.readFileSync('./../testImg.jpg')
+console.log('data:image/jpeg;base64,'+imageBuf.toString('base64'))
