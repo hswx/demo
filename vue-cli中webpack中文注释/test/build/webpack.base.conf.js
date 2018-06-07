bcbf53e5680b9714203b4a919f464eaa0a25998b
hswx <597084762@qq.com>
2018-06-07 23:36:51 +0800

    webpack.base.conf.js完成

--- vue-cli中webpack中文注释/test/build/webpack.base.conf.js
+++ vue-cli中webpack中文注释/test/build/webpack.base.conf.js
@@ -37,61 +37,81 @@ module.exports = {
     path: config.build.assetsRoot,
     filename: '[name].js', // 输出的js文件名
     publicPath: process.env.NODE_ENV === 'production'
+      // 配置最后开发/生产环境发布时静态资源所在的根目录
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
+    // 定义要resolve的文件的扩展名
     alias: {
+      // 定义路径别名
       'vue$': 'vue/dist/vue.esm.js',
+      // 以vue结尾的路径会被转到vue/dist/vue.esm.js
+      // 比如import Vue from 'vue/dist/vue.common.js'可以写成 import Vue from 'vue'
       '@': resolve('src'),
+      // @代表目录src
     }
   },
   module: {
     rules: [
       ...(config.dev.useEslint ? [createLintingRule()] : []),
+      // 根据配置是否开启eslint代码校验
       {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
+        // .vue文件使用vue-loader
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
         include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        // 对src，test，node_modules/webpack-dev-server/client下的js文件使用babel-loader
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        // 对图片等资源文件使用url-loader，放在img目录下，并指明输出的命名规则
         loader: 'url-loader',
         options: {
           limit: 10000,
+          // 10000字节以下的资源会被转成base64引用
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        // 对mp4等资源文件使用url-loader，放在media目录下，并指明输出的命名规则
         loader: 'url-loader',
         options: {
           limit: 10000,
+          // 10000字节以下的资源会被转成base64引用
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        // 对字体文件使用url-loader，放在fonts目录下，并指明输出的命名规则
         loader: 'url-loader',
         options: {
           limit: 10000,
+          // 10000字节以下的资源会被转成base64引用
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
     ]
   },
+  // 这些选项用于配置polyfill或mock某些node.js全局变量和模块。
+  // 这可以使最初为nodejs编写的代码可以在浏览器端运行
   node: {
     // prevent webpack from injecting useless setImmediate polyfill because Vue
     // source contains it (although only uses it if it's native).
+    // 这个配置是一个对象，其中的每个属性都是nodejs全局变量或模块的名称
     setImmediate: false,
+    // false表示什么都不提供。如果获取此对象的代码，可能会因为获取不到此对象而触发ReferenceError错误
     // prevent webpack from injecting mocks to Node native modules
     // that does not make sense for the client
+    // 设置成empty则表示提供一个空对象
     dgram: 'empty',
     fs: 'empty',
     net: 'empty',
